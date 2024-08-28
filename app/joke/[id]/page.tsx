d6188b55c26d0975620df50b75e742e91a512aea
Davod <davodnikoyii@gmail.com>
2024-08-28 10:21:35 +0200

    The page for displaying a joke with text and rating has been completed

--- app/joke/[id]/page.tsx
+++ app/joke/[id]/page.tsx
@@ -1,12 +1,22 @@
+import { jokes } from "@/app/data";
 
 interface Props{
     params: {id: string}
 }
 
 export default function JokePge(props: Props){
+    const joke = jokes.find((joke) => joke.id.toString() === props.params.id)
+
+    if(!joke){
+        return (<div>Joke not found</div>);
+    }    
     return (
-        <main className="min-h-screen">
-            {props.params.id}
+        <main className="flex flex-col items-center justify-center min-h-screen p-4">
+            <div className="text-center max-w-md w-full">
+                <h2 className="text-4xl font-bold mb-4">{joke.text}</h2>
+                <p className="text-xl text-gray-600">Rating: {joke.rating}</p>
+            </div>
         </main>
     );
 }
+        
